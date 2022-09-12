809f674aaa2f27ee52d93e6d99b7d1ffd9288671
hyun-jung-joo <80213599+hyun-jung-joo@users.noreply.github.com>
2022-09-12 12:33:12 +0900

    Add files via upload

---
+++ 2021_컴프/LABHW12/LAB12_1_4/LAB12_1_4.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int state;
+	FILE* fp;
+	char ch;
+
+	fp = fopen("hello.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("file open error!\n");
+		return 1;
+	}
+
+	fprintf(fp, "Hello\n");
+	fprintf(fp, "World\n");
+
+	state = fclose(fp);
+	if (state != 0) // 이 체크는 생략 가능
+	{
+		printf("file close error!\n");
+		return 1;
+	}
+
+	fp = fopen("hello.txt", "rt");
+	if (fp == NULL) {
+		printf("file open error!\n");
+		return 1;
+	}
+
+	ch = getc(fp);
+	while (!feof(fp)) {
+		putc(ch, stdout);
+		ch = getc(fp);
+	}
+	fclose(fp);
+}
